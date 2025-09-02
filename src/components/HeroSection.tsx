7873f79ffdcd49f7878feae4740b27cc349dbe4d
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-09-02 20:17:43 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,6 +11,11 @@ function HeroSection() {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
+
+      <Spotlight
+        className="-bottom-40 left-3/4 md:right-60 md:top-20 "
+        fill="white"
+      />
         <div className="p-4 relative z-10 w-full text-center" >
             <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
